1bb041b7861cdd3176511f86e942d32c891b1c92
Garry Fisher <igor.lytkin@gmail.com>
2018-11-26 04:12:17 +0300

    save

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,33 @@
 var clickplex = 1;
 var result = 0;
 
+window.onload = function(){
+	var res = localStorage.getItem('res');
+	console.log(res);
+	parsedresult=parseInt(res,10);
+	result = parsedresult;
+	var cp = localStorage.getItem('cp');
+	console.log(cp);
+	parcedcp=parseInt(cp,10);
+	clickplex = parcedcp;
+}
+
+
 
 function add(){
 	result = result+clickplex;
 	
 	document.getElementById('result').innerHTML =  result;
 	document.getElementById('clickplex').innerHTML =  clickplex;
-
+	localStorage.setItem('res', result);
+	localStorage.setItem('cp', clickplex);
 	if (result%100==0) {clickplex = clickplex +1;}
 
+}
+
+function reset(){
+	
+	localStorage.setItem('res', '0')
+	localStorage.setItem('cp', '1')
+	location.reload();
 }
